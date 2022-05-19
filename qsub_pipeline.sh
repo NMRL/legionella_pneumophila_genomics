3cb0fb1ba2d3680bb97df9d6aaffdaa7311f1e0e
omegatro <omegatro@gmail.com>
2022-05-19 13:33:30 +0300

    Fixed temp folder removal and sample id extraction issues

--- qsub_pipeline.sh
+++ qsub_pipeline.sh
@@ -3,7 +3,7 @@
 #PBS -l walltime=01:00:00
 #PBS -l procs=16
 #PBS -l pmem=2g
-#PBS -q batch
+#PBS -q fast
 #PBS -j oe
 #PBS -A rakus
 
@@ -14,7 +14,7 @@ cp $1 lpgenomics_temp/
 cp $2 lpgenomics_temp/
 R1_FASTQ=lpgenomics_temp/$(basename $1)
 R2_FASTQ=lpgenomics_temp/$(basename $2)
-SAMPLE=$(python -c "print('$R1_FASTQ'.split('_')[0])")
+SAMPLE=$(python -c "print('$(basename $1)'.split('_')[0])")
 
 if ( file $1 | grep -q compressed ) ; then
     singularity run image_files/pigz_latest.sif -d $R1_FASTQ $R2_FASTQ
@@ -22,4 +22,4 @@ fi
   
 bash pipeline.sh --reference=supportFiles/Phila_NC_002942.fna --gff=supportFiles/NC_002942.gff --r1=${R1_FASTQ::-3} --r2=${R2_FASTQ::-3} --isolate=$SAMPLE --output=./output
 
-rm -r ~/lpgenomics_temp
+rm -r lpgenomics_temp/
